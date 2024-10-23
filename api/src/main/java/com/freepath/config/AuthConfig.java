e4a16d375591b7f11f5e4aba4caf0311e6cf6947
nicolao00 <nicolao00@naver.com>
2024-10-23 17:03:29 +0900

    [Fix] 인증/인가 인터셉터 제거 및 롬북 의존성 수정

--- api/src/main/java/com/freepath/config/AuthConfig.java
+++ api/src/main/java/com/freepath/config/AuthConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.freepath.auth.component.jwt.JwtProvider;
-import com.freepath.support.interceptor.AuthInterceptor;
+//import com.freepath.support.interceptor.AuthInterceptor;
 import com.freepath.support.resolver.JwtResolver;
 
 @Configuration
@@ -22,8 +22,8 @@ public class AuthConfig implements WebMvcConfigurer {
     }
 
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor(jwtProvider))
-            .addPathPatterns("/v1/**");
+//        registry.addInterceptor(new AuthInterceptor(jwtProvider))
+//            .addPathPatterns("/v1/**");
     }
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
